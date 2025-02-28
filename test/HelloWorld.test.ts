4ac58520865bbfb024b52f0f82dc5fc715391cd0
Victor Moreno <victor.moreno@cabify.com>
2025-02-28 19:01:20 +0100

    chore: remove dead code

--- test/HelloWorld.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect } from "chai";
-import { HelloWorld } from "../src/HelloWorld.ts";
-
-it("hello world", () => {
-  expect(new HelloWorld().sayHello()).to.equal("Hello World!");
-});
-
