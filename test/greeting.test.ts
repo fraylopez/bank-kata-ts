6d3d4e3e959b3f1e36248e249068ded08d1da4fa
Victor Moreno <victor.moreno@cabify.com>
2025-02-28 19:05:04 +0100

    test: bookmark

--- test/greeting.test.ts
+++ test/greeting.test.ts
@@ -5,6 +5,10 @@ it("interpolates name", () => {
   expect(greet("Alice")).to.equal("Hello, Alice.");
 });
 
+it("handle null", () => {
+  expect(greet(null)).to.equal("Hello, my friend.");
+});
+
 function greet(who: string) {
   return `Hello, ${who}.`;
 }
