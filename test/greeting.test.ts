e57347fabde96602a9019cda9213686b53070165
Victor Moreno <victor.moreno@cabify.com>
2025-02-28 19:16:41 +0100

    feat: handle null

--- test/greeting.test.ts
+++ test/greeting.test.ts
@@ -9,6 +9,9 @@ it("handle null", () => {
   expect(greet(null)).to.equal("Hello, my friend.");
 });
 
-function greet(who: string) {
+function greet(who: string | null): string {
+  if (who === null) {
+    return "Hello, my friend.";
+  }
   return `Hello, ${who}.`;
 }
