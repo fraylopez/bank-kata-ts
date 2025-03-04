fcc574e7911b45b72a3a8687f1e3842c2a273870
Victor Moreno <victor.moreno@cabify.com>
2025-03-04 17:10:16 +0100

    refactor: extract isArray function

--- src/greet.ts
+++ src/greet.ts
@@ -1,25 +1,27 @@
 type Greetable = string[] | string | null;
 
 export function greet(who: Greetable): string {
-  if (Array.isArray(who)) {
-    return handleArray(who);
+  if (isArray(who)) {
+    return handleArray(who as string[]);
   }
   if (who === null) {
     return "Hello, my friend.";
   }
-  if (isShouting(who)) {
+  if (isShouting(who as string)) {
     return `HELLO, ${who}!`;
   }
 
   return `Hello, ${who}.`;
 }
 
-function isShouting(who: string): boolean {
-  return who === who.toUpperCase();
+function isArray(who: Greetable): boolean {
+  return Array.isArray(who);
 }
-
 
 function handleArray(who: string[]): string {
   return `Hello, ${who.slice(0, -1).join(", ")} and ${who.slice(-1)}`;
 }
 
+function isShouting(who: string): boolean {
+  return who === who.toUpperCase();
+}
